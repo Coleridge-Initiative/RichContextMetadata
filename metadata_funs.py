8de044100e0beb88f3422337ec42edee9a8c404e
Sophie Rand <sophierand@10-18-236-44.dynapool.wireless.nyu.edu>
2019-08-13 10:28:58 -0400

    adding manually curated dataset names

--- metadata_funs.py
+++ metadata_funs.py
@@ -95,3 +95,12 @@ def read_datasets():
         dataset_json = json.load(json_file)
 #     dataset_names =[{'dataset_name':d['title'],'dataset_id':d['dataset_id']} for d in dataset_json]
     return dataset_json
+
+
+def read_pubs():
+    publication_json_path = os.path.join(os.getcwd(),'metadata/publications_lim.json')
+
+    with open(publication_json_path) as json_file:
+        publications_json = json.load(json_file)
+#     dataset_names =[{'dataset_name':d['title'],'dataset_id':d['dataset_id']} for d in dataset_json]
+    return publications_json
